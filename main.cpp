e995536028e12bc5578c4fb029a7f1ae15416349
SoneTaisei <k024g0009@g.neec.ac.jp>
2025-04-18 10:45:43 +0900

    座標変換以外のところを完成させた。

--- main.cpp
+++ main.cpp
@@ -1,6 +1,49 @@
 #include <Novice.h>
+#include <assert.h>
 
 const char kWindowTitle[] = "LC1C_10_ソネ_タイセイ_タイトル";
+
+/*********************************************************
+*構造体
+*********************************************************/
+
+// 4x4の行列
+struct Matrix4x4 {
+	float m[4][4];
+};
+
+// vector3
+struct Vector3 {
+	float x;
+	float y;
+	float z;
+};
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 44;
+
+/*********************************************************
+*関数
+*********************************************************/
+
+/*行列の描画
+*********************************************************/
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4 &matrix);
+
+void VectorScreenPrintf(int x, int y, Vector3 vector);
+
+/*行列の計算
+*********************************************************/
+
+// 平行移動行列
+Matrix4x4 MakeTranslateMatrix(const Vector3 &translate);
+
+// 拡大縮小行列
+Matrix4x4 MakeScaleMatrix(const Vector3 &scale);
+
+// 座標変換
+Vector3 Transform(const Vector3 &vector, const Matrix4x4 &matrix);
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -9,11 +52,24 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
+
+	Vector3 translate{ 4.1f,2.6f,0.8f };
+	Vector3 scale{ 1.5f,5.2f,7.3f };
+	Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+	Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
+	Vector3 point{ 2.3f,3.8f,1.4f };
+	Matrix4x4 transformMatrix{
+		1.0f,2.0f,3.0f,4.0f,
+		3.0f,1.0f,1.0f,2.0f,
+		1.0f,4.0f,2.0f,3.0f,
+		2.0f,2.0f,1.0f,3.0f,
+	};
+	Vector3 transformed = Transform(point, transformMatrix);
 
 	// ウィンドウの×ボタンが押されるまでループ
-	while (Novice::ProcessMessage() == 0) {
+	while(Novice::ProcessMessage() == 0) {
 		// フレームの開始
 		Novice::BeginFrame();
 
@@ -32,7 +88,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-
+		VectorScreenPrintf(0, 0, transformed);
+		MatrixScreenPrintf(0, kRowHeight * 5, translateMatrix);
+		MatrixScreenPrintf(0, kRowHeight*10, scaleMatrix);
 		///
 		/// ↑描画処理ここまで
 		///
@@ -41,7 +99,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		Novice::EndFrame();
 
 		// ESCキーが押されたらループを抜ける
-		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+		if(preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
 			break;
 		}
 	}
@@ -49,4 +107,66 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの終了
 	Novice::Finalize();
 	return 0;
+}
+
+/*行列の描画
+*********************************************************/
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4 &matrix) {
+	for(int row = 0; row < 4; ++row) {
+		for(int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(
+				x + column * kColumnWidth,
+				y + row * kRowHeight,
+				"%7.02f", matrix.m[row][column]);
+		}
+	}
+}
+
+void VectorScreenPrintf(int x, int y, Vector3 vector) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
+}
+
+
+Matrix4x4 MakeTranslateMatrix(const Vector3 &translate) {
+	Matrix4x4 result = {};
+
+	result = {
+		1.0f,0.0f,0.0f,0.0f,
+		0.0f,1.0f,0.0f,0.0f,
+		0.0f,0.0f,1.0f,0.0f,
+		translate.x,translate.y,translate.z,1.0f
+	};
+
+	return result;
+}
+
+Matrix4x4 MakeScaleMatrix(const Vector3 &scale) {
+	Matrix4x4 result = {};
+
+	result = {
+		scale.x,0.0f,0.0f,0.0f,
+		0.0f,scale.y,0.0f,0.0f,
+		0.0f,0.0f,scale.z,0.0f,
+		0.0f,0.0f,0.0f,1.0f,
+	};
+
+	return result;
+}
+
+// 座標変換はできていない
+
+Vector3 Transform(const Vector3 &vector, const Matrix4x4 &matrix) {
+	Vector3 result;
+
+	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + 1.0f * matrix.m[2][0];
+	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + 1.0f * matrix.m[2][1];
+	float w = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + 1.0f * matrix.m[2][2];
+	assert(w != 0.0f);
+	result.x /= w;
+	result.y /= w;
+
+	return result;
 }
