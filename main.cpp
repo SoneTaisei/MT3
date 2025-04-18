4a94e8594103c017729d4461a0ba61f10b8b6ca2
SoneTaisei <k024g0009@g.neec.ac.jp>
2025-04-18 12:36:42 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -62,6 +62,9 @@ Matrix4x4 MakeScaleMatrix(const Vector3 &scale);
 // 座標変換
 Vector3 Transform(const Vector3 &vector, const Matrix4x4 &matrix);
 
+// アフィン変換
+Matrix4x4 MakeAffineMatrix(const Vector3 &scale, const Vector3 &rotate, const Vector3 &translate);
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
@@ -71,6 +74,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
+
+	Vector3 scale = { 1.2f,0.79f,-2.1f };
+	Vector3 rotate = { 0.4f,1.43f,-0.8f };
+	Vector3 translate = { 2.7f,-4.15f,1.57f };
+	Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotate, translate);
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while(Novice::ProcessMessage() == 0) {
@@ -92,7 +100,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-    
+		MatrixScreenPrintf(0, 0, worldMatrix);
 		///
 		/// ↑描画処理ここまで
 		///
@@ -152,7 +160,20 @@ Matrix4x4 MakeRoteYMatrix(float radian) {
 		0.0f,1.0f,0.0f,0.0f,
 		std::sin(radian),0.0f,std::cos(radian),0.0f,
 		0.0f,0.0f,0.0f,1.0f
-      };
+	};
+
+	return result;
+}
+
+Matrix4x4 MakeRoteZMatrix(float radian) {
+	Matrix4x4 result{};
+
+	result = {
+		std::cos(radian),std::sin(radian),0.0f,0.0f,
+		-std::sin(radian),std::cos(radian),0.0f,0.0f,
+		0.0f,0.0f,1.0f,0.0f,
+		0.0f,0.0f,0.0f,1.0f
+	};
 
 	return result;
 }
@@ -170,19 +191,6 @@ Matrix4x4 MakeTranslateMatrix(const Vector3 &translate) {
 		0.0f,0.0f,1.0f,0.0f,
 		translate.x,translate.y,translate.z,1.0f
 	};
-
-	return result;
-}
-
-Matrix4x4 MakeRoteZMatrix(float radian) {
-	Matrix4x4 result{};
-
-	result = {
-		std::cos(radian),std::sin(radian),0.0f,0.0f,
-		-std::sin(radian),std::cos(radian),0.0f,0.0f,
-		0.0f,0.0f,1.0f,0.0f,
-		0.0f,0.0f,0.0f,1.0f
-      };
 
 	return result;
 }
@@ -230,4 +238,25 @@ Vector3 Transform(const Vector3 &vector, const Matrix4x4 &matrix) {
 	result.z /= w;
 
 	return result;
+}
+
+// アフィン変換
+Matrix4x4 MakeAffineMatrix(const Vector3 &scale, const Vector3 &rotate, const Vector3 &translate) {
+	Matrix4x4 result{};
+
+	Matrix4x4 rotX = MakeRoteXMatrix(rotate.x);
+	Matrix4x4 rotY = MakeRoteYMatrix(rotate.y);
+	Matrix4x4 rotZ = MakeRoteZMatrix(rotate.z);
+	
+	Matrix4x4 rotateMatrix=
+
+	result = {
+		scale.x * rotate.x,scale.x * rotate.x,scale.x * rotate.x,0.0f,
+		scale.y * rotate.y,scale.y * rotate.y,scale.y * rotate.y,0.0f,
+		scale.z * rotate.z,scale.z * rotate.z,scale.z * rotate.z,0.0f,
+		translate.x,translate.y,translate.z,1.0f
+	};
+
+	return result;
+
 }
